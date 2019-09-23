f3d134ce8df1b877364d544946ae8d58c7d28f78
witkowt2 <tomasz.witkowski.tw2@contractors.roche.com>
2019-09-23 08:21:17 +0200

    Add project files.

---
+++ todo/Data/todoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace todo.Models
+{
+    public class todoContext : DbContext
+    {
+        public todoContext (DbContextOptions<todoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<todo.Models.Task> Task { get; set; }
+    }
+}
---
+++ todo/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DeadlineDate { get; set; }
+        public string Description { get; set; }
+    }
+}
